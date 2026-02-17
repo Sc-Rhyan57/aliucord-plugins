3a93f2c1f5e6426cb852616f07895f0633417c5f
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-17 03:01:35 -0300

    Add files via upload

--- plugin/SvClone/build.gradle.kts
+++ plugin/SvClone/build.gradle.kts
@@ -1,11 +1,15 @@
-version = "1.0.1"
-description = "Clona servidores Discord. | Baseado em https://bettercloner.vercel.app"
+version = "1.0.2"
+description = "Cloning Discord servers. | Based on https://bettercloner.vercel.app"
 
 aliucord {
     author("Rhyan57", 896604349311115304L)
 
     changelog.set(
         """
+            # 1.0.2
+            * No more crashes
+            * translated into English
+
             # 1.0.1
             * Corrigido erro de Iterator
             * GUI mais arredondada
