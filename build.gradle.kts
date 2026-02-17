d55e7f32fdc853d49a4a4e483c78d551dd0e5343
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-16 21:32:34 -0300

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.aliucord)
-    alias(libs.plugins.ktlint) apply false
-}
