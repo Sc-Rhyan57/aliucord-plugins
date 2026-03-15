943d21de8138b3ecf4c034bd2f8a3d090247dfd2
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-03-15 15:20:56 -0300

    feat: upload files via Android Dev Tools

---
+++ plugin/ViewQuests/build.gradle.kts
@@ -0,0 +1,23 @@
+version = "2.0.0"
+
+description = "View, complete and claim Discord Quests with Orb balance display"
+
+aliucord {
+    author("rhyan57", 896604349311115304L)
+    changelog.set("""
+        # 2.0.0:
+        - Complete rewrite
+        - Auto-complete quests (video, activity)
+        - Claim Reward button with captcha support
+        - Orb balance on profile
+        - Animated gradient on Quests button
+        - Improved Collectibles with images
+        - Modern Discord-style UI
+
+        # 1.0.1:
+        - Minor fixes
+
+        # 1.0.0:
+        - Initial release
+        """.trimIndent())
+}
